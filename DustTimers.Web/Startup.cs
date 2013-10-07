31b614a38d4f7a61476feba49b628b4440abd282
David Burela <David.Burela@gmail.com>
2013-10-07 18:36:53 +1100

    Added blank ASP.Net MVC 5 & unit test projects

---
+++ DustTimers.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DustTimers.Web.Startup))]
+namespace DustTimers.Web
+{
+    public partial class Startup 
+    {
+        public void Configuration(IAppBuilder app) 
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
