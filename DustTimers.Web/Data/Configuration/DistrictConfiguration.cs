11be0eabc85ae04025b56be197c0330359a436f2
David Burela <David.Burela@gmail.com>
2013-10-08 11:27:37 +1100

    Got EF Code first up working
    I still need to define the relationships, etc.

---
+++ DustTimers.Web/Data/Configuration/DistrictConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using CrestParser.Models;
+
+namespace DustTimers.Web.Data.Configuration
+{
+    public class DistrictConfiguration : EntityTypeConfiguration<District>
+    {
+        public DistrictConfiguration()
+        {
+            //TODO: Add the relationships
+        }
+    }
+}
---
+++ DustTimers.Web/Data/DustTimerDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using CrestParser.Models;
+using DustTimers.Web.Data.Configuration;
+
+namespace DustTimers.Web.Data
+{
+    public class DustTimerDbContext : DbContext
+    {
+
+        public DustTimerDbContext()
+            : base("DefaultConnection") { }
+        
+        // Tables
+        public DbSet<District> Districts { get; set; }
+        public DbSet<Infrastructure> Infrastructures { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Planet> Planets { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<CrestParser.Models.System> Systems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("DustTimers");
+        }
+    }
+
+}
