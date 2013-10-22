370b7b65dbfc50971617d7d5e41d0d91cef4e24a
David Burela <David.Burela@gmail.com>
2013-10-22 23:33:51 +1100

    Created new custom route for quick Corp lookup
    It is now possible to go http://DustTimers.com/[CorpTicker]
    This lets anyone quickly bring up the list of district defences for a
    corp.

--- DustTimers.Web/App_Start/RouteConfig.cs
+++ DustTimers.Web/App_Start/RouteConfig.cs
@@ -14,10 +14,18 @@ namespace DustTimers.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Ticker",
+                url: "{id}",
+                defaults: new { controller = "District", action = "Ticker"}
+            ); 
+            
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            
         }
     }
 }
