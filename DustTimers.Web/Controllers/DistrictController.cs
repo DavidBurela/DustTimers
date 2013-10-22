a140ab7235296e47841a74fb16e7daa5807eea97
David Burela <David.Burela@gmail.com>
2013-10-22 21:24:08 +1100

    Displaying districts for a corp
    You can now enter a corp ticker in the url /District/Ticker/TICKER
    This will display all the districts a corporation owns.
    Next I will clean up the display, to show upcoming battles, and then a
    list of districts.

---
+++ DustTimers.Web/Controllers/DistrictController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CrestParser.Models;
+using DustTimers.Web.Data;
+using DustTimers.Web.Repositories.Uow;
+
+namespace DustTimers.Web.Controllers
+{
+    public class DistrictController : Controller
+    {
+        public DustTimersUow DustTimersUow { get; set; }
+
+        public DistrictController()
+        {
+            DustTimersUow = new DustTimersUow();
+        }
+
+        // GET: /District/
+        public async Task<ActionResult> Index()
+        {
+            return View(await DustTimersUow.DistrictRepository.GetAll().ToListAsync());
+        }
+
+        // GET: /District/Ticker/5
+        public async Task<ActionResult> Ticker(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var corporation = await DustTimersUow.CorporationRepository.GetAll().Where(p => string.Compare(p.Ticker, id, StringComparison.OrdinalIgnoreCase) == 0).FirstOrDefaultAsync();
+            if (corporation == null)
+            {
+                return HttpNotFound();
+            }
+
+            var districts = await DustTimersUow.DistrictRepository.GetAll().Where(p => p.Owner.Id == corporation.Id).ToListAsync();
+            if (districts == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Index", districts);
+        }
+
+    }
+}
