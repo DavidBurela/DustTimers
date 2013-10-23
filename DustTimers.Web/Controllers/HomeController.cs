59e19406881b3ea3e2789cb0abe2467db4aae004
David Burela <David.Burela@gmail.com>
2013-10-23 18:34:05 +1100

    Corporation list on /Home screen

--- DustTimers.Web/Controllers/HomeController.cs
+++ DustTimers.Web/Controllers/HomeController.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using CrestParser.Models;
+using DustTimers.Web.Models;
+using DustTimers.Web.Repositories.Uow;
+using WebGrease.Css.Extensions;
 
 namespace DustTimers.Web.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public DustTimersUow DustTimersUow { get; set; }
+
+        public HomeController()
         {
-            return View();
+            DustTimersUow = new DustTimersUow();
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var corpDistrictDetails = await DustTimersUow.DistrictRepository.GetAllWithDetails().Where(p => p.Owner != null).GroupBy(p => p.Owner.Id)
+                .Select(p => new CorpDistrictDetail{CorporationId = p.Key, CorporationName = p.FirstOrDefault().Owner.Name, DistrictsTotal = p.Count(), DistrictsUnderAttack = p.Count(q => q.Attacked)}).ToListAsync();
+            var corpIds = corpDistrictDetails.Select(p => p.CorporationId);
+            var corporations = await DustTimersUow.CorporationRepository.GetAll().Where(p => corpIds.Contains(p.Id)).ToListAsync();
+
+            foreach (var corpDistrictDetail in corpDistrictDetails)
+            {
+                CorpDistrictDetail detail = corpDistrictDetail;
+                var corporation = corporations.FirstOrDefault(p => p.Id == detail.CorporationId);
+                if (corporation != null)
+                    corpDistrictDetail.Ticker = corporation.Ticker;
+            }
+
+            var viewModel = new HomeViewModel();
+            viewModel.CorpDistrictDetails = corpDistrictDetails;
+
+            return View(viewModel);
         }
 
         public ActionResult About()
@@ -18,11 +47,5 @@ namespace DustTimers.Web.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
---
+++ DustTimers.Web/Models/HomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CrestParser.Models;
+using DustTimers.LegacyApi.Models;
+
+namespace DustTimers.Web.Models
+{
+    public class HomeViewModel
+    {
+        public List<CorpDistrictDetail> CorpDistrictDetails { get; set; }
+    }
+
+    public class CorpDistrictDetail
+    {
+        public int CorporationId { get; set; }
+        public string CorporationName { get; set; }
+        public string Ticker { get; set; }
+        public int DistrictsTotal { get; set; }
+        public int DistrictsUnderAttack { get; set; }
+    }
+}
